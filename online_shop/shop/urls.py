c358367187e1432785eb9f60903853604de73a65
adviser_ua <kostyamail05@gmail.com>
2019-05-29 20:01:12 +0300

    add search, pagination

--- online_shop/shop/urls.py
+++ online_shop/shop/urls.py
@@ -2,10 +2,14 @@
 # Konstantyn Davidenko
 
 from django.conf.urls import url
+from django.urls import re_path
+
 from . import views
 
 urlpatterns = [
-    url('list/$', views.ProductList, name='ProductList'),
+    # url('list/$', views.ProductList, name='ProductList'),
+    url('list/$', views.AllProduct.as_view(), name='ProductList'),
     url('(?P<id>\d+)$', views.ProductDetail, name='ProductDetail'),
+    re_path(r'list/?', views.Search.as_view(), name='search'),
     url('^$', views.MainPage, name='home'),
 ]
--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -1,9 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, get_object_or_404, render_to_response
 
 from cart.cart import Cart
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.generic.list import MultipleObjectMixin, ListView
+
 from .models import Product
 
 
@@ -26,3 +31,42 @@ def ProductDetail(request, id):
     # cart_product_form = CartAddProductForm()
     # return render(request, 'shop/product/detail.html',{'product': product,'cart_product_form': cart_product_form})
     return render(request, 'shop/product/detail.html', {'product': product})
+
+
+class AllProduct(TemplateView):
+    template_name = 'shop/product/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = Product.objects.all()
+        paginator = Paginator(products, 6)  # Show 25 contacts per page
+        page = self.request.GET.get('page')
+        page_products = paginator.get_page(page)
+        context['products'] = page_products
+        return context
+
+
+class Search(TemplateView):
+    template_name = 'shop/product/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get('name')
+        products = Product.objects.filter(name__contains=name)
+        paginator = Paginator(products, 6)  # Show 25 contacts per page
+        page = self.request.GET.get('page')
+        page_products = paginator.get_page(page)
+        context['products'] = page_products
+        context['queries'] = products
+        return context
+
+
+from django import template
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.update(kwargs)
+    return query.urlencode()
